34d7811e54b884b07106b671f1c91df91016c3ec
Ravi Bhalala <ravi.bhalala@nium.com>
2025-06-27 16:56:43 +0200

    updated cover page

--- app/components/Me/Me.tsx
+++ app/components/Me/Me.tsx
@@ -6,20 +6,21 @@ const Me = () => {
   return (
     <div className={styles['me']}>
       <div className={styles['me-cover']}>
-        <Image src="./cover.jpeg" alt="Cover Image" layout="responsive" />
+        <Image src="/cover.png" width={700} height={375} alt="Cover Image" layout="responsive" />
       </div>
       <div className={styles['me-data']}>
         <div className={styles['me-data-profile-pic']}>
           <Image
-            src="./profile.jpeg"
+            src="/profile.jpeg"
             alt="Cover Image"
+            width={400} height={400}
             className={styles['image']}
           />
         </div>
         <div className={styles['name-container']}>
           <h2 className={styles['name']}>Ravi Bhalala</h2>
           <h4 className={styles['profession']}>
-            Staff Software Engineer at Nium | Full-Stack | Fintech | Payments Expert | Get Things Done
+            Staff Software Engineer | Full-Stack | Fintech | Payments Expert | Get Things Done
           </h4>
         </div>
       </div>
